526cd3a1045d45d7dc3902b858c4590115c6ee3e
shreyasvk <147273427+shreyasvk@users.noreply.github.com>
2024-07-15 12:17:31 +0530

    Delete App-Configuration directory

--- App-Configuration/databases/amenities.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from bson import ObjectId
-from pydantic_core import core_schema
-
-MONGO_URL = "mongodb+srv://Shreyas:Shreyas123@fasterapi.i2bp8f5.mongodb.net/"
-client = AsyncIOMotorClient(MONGO_URL)
-database = client.amenities_db
-amenities_collection = database.amenities
